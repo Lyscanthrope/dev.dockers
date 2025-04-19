f32d7aff4477ee56c68ab571fe922b334aac8123
Remi Bonidal <rbonidal@gmail.com>
2025-04-19 20:24:41 +0000

    addition of helix

--- scripts/install_rustup.sh
+++ scripts/install_rustup.sh
@@ -1,6 +1,15 @@
 sudo apt update
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt update
 sudo apt install build-essential -y
-sudo apt-get install pkg-config openssl libssl-dev
-curl https://sh.rustup.rs -sSf | bash -s -- -y
-sudo cargo install typst-cli
-echo 'source $HOME/.cargo/env' >> $HOME/.bashrc
+sudo apt install helix -y
+#sudo apt-get install pkg-config openssl libssl-dev
+#curl https://sh.rustup.rs -sSf | bash -s -- -y
+#echo 'source $HOME/.cargo/env' >> $HOME/.bashrc
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+. "$HOME/.cargo/env"
+cargo install typst-cli
+cargo install iwe
+cargo install iwes
+cargo install --git https://github.com/nvarner/typst-lsp typst-lsp
